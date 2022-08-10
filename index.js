8ac33a56ae4484a88df1ed30f195756fc1312b18
HarisPratama <hpratama324@gmail.com>
2022-08-10 20:54:45 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true, bodyParser: true}))
+app.use(express.urlencoded({extended: true}))
 
 app.use(router)
 
